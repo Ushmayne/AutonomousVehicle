e25c2efd747fde066c2a7a609d69640d01c3af80
Rutu <darji1@uwindsor.ca>
2021-12-24 13:00:04 -0500

    some movement controlling through script

---
+++ AutonomousVehicleProject/Assets/Scripts/VehicleController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+	
+	//moves vehicle forward by distance specified ("forward" refers to current facing direction)
+	public void MoveForward(float distance){
+		transform.position += transform.forward.normalized * distance; 
+	}
+	
+	//turns vehicle (on y axis) depending on inputted degrees (left and right turns depends on if inputted degrees are negative or positive)
+    public void Turn(float deg){
+		transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + deg, transform.rotation.z);
+	}
+}
