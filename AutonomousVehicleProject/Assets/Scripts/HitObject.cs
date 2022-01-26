182a9b7d8a7bb66b7ba2a24e6113bf6d78eb9f88
Massimo Albano <67909039+massimoa15@users.noreply.github.com>
2022-01-26 15:34:58 -0500

    Created Radar Sensor
    Created a radar sensor script and prefab which scans continually in a circle similar to the SimpleSensor.cs but it keeps a list of the objects it finds every revolution around in a circle (their distance and angle)

---
+++ AutonomousVehicleProject/Assets/Scripts/HitObject.cs
@@ -0,0 +1,16 @@
+﻿public class HitObject
+{
+    public float distance;
+    public float angle;
+
+    public HitObject(float distance, float angle)
+    {
+        this.distance = distance;
+        this.angle = angle;
+    }
+
+    public override string ToString()
+    {
+        return "Distance: " + distance + ". Angle: " + angle;
+    }
+}
